b23a79c4df9101513c2607dad66c2122b7058747
Laika Felshark <nope@nope.nope>
2022-05-18 17:10:46 -0400

    added physics

--- sand.c
+++ sand.c
@@ -1,10 +1,3 @@
-/*
-A simple example of using the gfx library.
-CSE 20211
-9/7/2011
-by Prof. Thain
-*/
-
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/types.h>
@@ -14,18 +7,43 @@ by Prof. Thain
 
 const char *pDevice = "/dev/input/mice";
 
+void swapGrain(int uA[100][100], int uB[100][100], int xA, int yA, int xB, int yB)
+{
+  int tmpA;
+  int tmpB;
+  tmpA = uA[xA][yA];
+  tmpB = uA[xB][yB];
+  uB[xA][yA] = tmpB;
+  uB[xB][yB] = tmpA;
+}
+
 int main()
 {
-	int ysize = 300;
-	int xsize = 300;
-  int i = 0;
-	char event;
+  int ysize = 300;
+  int xsize = 300;
+  int i,j;
+  char event;
   int fd;
   int bytes;
   unsigned char data[3];
-  int left, middle, right;
-  int toggle = 0;
+  int toggleLeft = 0;
+  int toggleRight = 0;
+  int brush = 1;
 
+
+  int universe[100][100];
+  int universePage[100][100];
+  //initialize universe
+  for(i = 0; i < 100; i++)
+  {
+    for(j = 0; j < 100; j++)
+    {
+      universe[i][j] = 0;
+      universePage[i][j] = 0;
+    }
+  }
+  
+  //grab mouse
   fd = open(pDevice, O_RDONLY | O_NONBLOCK);
   if (fd == -1) {
     printf("ERROR Opening %s\n", pDevice);
@@ -36,15 +54,32 @@ int main()
 	// Open a new window for drawing.
 	gfx_open(xsize,ysize,"SAND II: GRITTIER");
 
-	// Set the current drawing color to white.
-  gfx_color(0xFF,0xFF,0xFF);
-	while(1) {
+  while(1) {
+    
     //grab key events 
 		if(gfx_event_waiting())
     {
       event = gfx_wait();
+      if(event =='+') brushSize++;
+      if(event =='-') brushSize--;
       if(event =='q') break;
+      if(event =='r')
+      {
+        for(i = 0; i < 100; i++)
+        {
+          for(j = 0; j < 100; j++)
+          {
+            universe[i][j] = 0;
+            universePage[i][j] = 0;
+          }
+        }
+      }
+      if(event =='1')
+        brush = 1;
+      if(event =='2')
+        brush = 2;
     }
+    
     
     //grab raw mouse data
     bytes = read(fd, data, sizeof(data));
@@ -52,27 +87,92 @@ int main()
     {
       //left click down
       if(data[0] == 0x9)
-        toggle = 1;
+        toggleLeft = 1;
       //left click up
       if(data[0] == 0x8)
-        toggle = 0;
+        toggleLeft = 0;
+      //right click down
+      if(data[0] == 0xa)
+        toggleRight = 1;
+      //right click up
+      if(data[0] == 0x8)
+        toggleRight = 0;
     }
- 
-    if(toggle)
-    {
-      gfx_point(gfx_mousex()+0, gfx_mousey()+0);
-      gfx_point(gfx_mousex()+0, gfx_mousey()+1);
-      gfx_point(gfx_mousex()+0, gfx_mousey()+2);
 
-      gfx_point(gfx_mousex()+1, gfx_mousey()+0);
-      gfx_point(gfx_mousex()+1, gfx_mousey()+1);
-      gfx_point(gfx_mousex()+1, gfx_mousey()+2);
+    
+    //interaction with universe
+    if(toggleLeft)
+    {
+      universe[gfx_mousex()/3][gfx_mousey()/3] = brush;
+    }
+    else if (toggleRight)
+    {
+      universe[gfx_mousex()/3][gfx_mousey()/3] = 0;
+    }
+    
+
+    //update universe
+    for(i = 0; i < 100; i++)
+    {
+      for(j = 0; j < 100; j++)
+      {
+        //bottom row is a sink
+        if(j == 99 || i == 0 || i == 99)
+          universePage[i][j] = 2;
+        else if (universe[i][j] == 1 && universe[i][j+1] == 0)
+        {
+          swapGrain(universe, universePage, i, j, i, j+1);
+        }
+        else if (universe[i][j] == 1 && universe[i+1][j+1] == 0)
+        {
+          swapGrain(universe, universePage, i, j, i+1, j+1);
+        }
+        else if (universe[i][j] == 1 && universe[i-1][j+1] == 0)
+        {
+          swapGrain(universe, universePage, i, j, i-1, j+1);
+        }
+        else if (universe[i][j] == 2)
+        {
+          universePage[i][j] = 2;
+        }
+      }
+    }
+    for(i = 0; i < 100; i++)
+    {
+      for(j = 0; j < 100; j++)
+      {
+        universe[i][j] = universePage[i][j];
+      }
+    }
+    
+
+    //draw universe
+    for(i = 0; i < 100; i++)
+    {
+      for(j = 0; j < 100; j++)
+      {
+        if(universe[i][j] == 0)
+          gfx_color(0x00,0x00,0x00);
+        if(universe[i][j] == 1)
+          gfx_color(0xff,0xff,0xff);
+        if(universe[i][j] == 2)
+          gfx_color(0xff,0xb0,0xb0);
+        
+        gfx_point(i*3+0, j*3+0);
+        gfx_point(i*3+0, j*3+1);
+        gfx_point(i*3+0, j*3+2);
+
+        gfx_point(i*3+1, j*3+0);
+        gfx_point(i*3+1, j*3+1);
+        gfx_point(i*3+1, j*3+2);
       
-      gfx_point(gfx_mousex()+2, gfx_mousey()+0);
-      gfx_point(gfx_mousex()+2, gfx_mousey()+1);
-      gfx_point(gfx_mousex()+2, gfx_mousey()+2);
+        gfx_point(i*3+2, j*3+0);
+        gfx_point(i*3+2, j*3+1);
+        gfx_point(i*3+2, j*3+2);
+      }
     }
     gfx_flush();
+    usleep(50000);
 	}
 
 	return 0;
